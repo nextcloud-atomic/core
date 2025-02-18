10a9f5a753efd3681f76e4198559e56285b3ec95
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2025-02-18 01:08:09 +0100

    grpc-journal: Add support for grpc-web and disable CORS for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-use std::fmt;
+use std::collections::HashMap;
+use std::{fmt, thread};
 use std::fmt::Display;
-use tonic::Status;
+use std::net::ToSocketAddrs;
+use tokio::sync::mpsc::Receiver;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio_stream::{Stream, StreamExt};
+use tonic::{Request, Response, Status};
+use api::{LogMessage, LogFilter, journal_log_stream_server::JournalLogStream};
+
+pub mod api {
+    tonic::include_proto!("api");
+}
 
 #[derive(Debug)]
 pub enum NcAtomicError {
@@ -92,3 +102,154 @@ impl From<NcAtomicError> for Status {
         }
     }
 }
+
+#[derive(Debug, Default)]
+pub struct JournalLogStreamService {
+    user_logs: bool,
+    system_logs: bool
+}
+
+impl JournalLogStreamService {
+    pub fn new(user_logs: bool, system_logs: bool) -> JournalLogStreamService {
+        JournalLogStreamService{ user_logs, system_logs }
+    }
+}
+
+#[tonic::async_trait]
+impl JournalLogStream for JournalLogStreamService {
+    type TailStream = ReceiverStream<Result<LogMessage, Status>>;
+
+    async fn tail(&self, request: Request<LogFilter>) -> Result<Response<Self::TailStream>, Status> {
+        #[cfg(debug_assertions)]
+        {
+            println!("Received tail request: {:?}", request);
+        }
+
+        let filter = request.into_inner();
+
+        let rx = get_log_stream(self.user_logs, self.system_logs, filter.namespace, Some(filter.fields)).await;
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
+
+pub async fn get_log_stream(current_user: bool, system: bool, namespace: Option<String>, filter_fields: Option<HashMap<String, String>>) -> Receiver<Result<LogMessage, Status>> {
+    println!("getting log stream");
+
+    let (tx, rx) = tokio::sync::mpsc::channel(100);
+    let default_msg = "<no message>".to_string();
+
+    const KEY_UNIT: &str = "_SYSTEMD_UNIT";
+    const KEY_MESSAGE: &str = "MESSAGE";
+
+    thread::spawn(move || {
+        use systemd::journal::{self, JournalSeek};
+        let filter_fields = filter_fields.unwrap_or_default();
+        let mut opts = journal::OpenOptions::default()
+            .system(system)
+            .current_user(current_user)
+            .system(system).to_owned();
+        let reader_result = match &namespace {
+            None => opts
+                .all_namespaces(true)
+                .open(),
+            Some(ns) => opts.open_namespace(ns)
+        };
+        let mut reader = match reader_result {
+            Err(e) => {
+                eprintln!("Couldn't open journal: {e:?}");
+                return;
+            },
+            Ok(reader) => reader
+        };
+        for (k, v) in filter_fields.iter() {
+            if let Err(e) = reader.match_add(k, v.as_bytes()) {
+                eprintln!("Error adding filter: {e:?}");
+            }
+        }
+
+        reader.seek(JournalSeek::Tail).expect("Couldn't seek to end of journal");
+        if let Err(e) = reader.previous().map_err(|e| NcAtomicError::IOError(e.to_string())) {
+            eprintln!("{e:?}");
+            return;
+        };
+
+        let mut abort = false;
+        loop {
+            loop {
+                match reader.next() {
+                    Ok(0) => {
+                        // if let Err(e) = tx.blocking_send(Err(Status::cancelled("no more messages found"))) {
+                        //     eprintln!("An error occurred while cancelling stream: {e:?}");
+                        // }
+                        break;
+                    },
+                    Ok(_) => {},
+                    Err(e) => {
+                        if let Err(e2) = tx.blocking_send(Err(Status::cancelled("unexpected error"))) {
+                            eprintln!("Unexpected error: {e:?}\n Also, an error occurred while cancelling stream: {e2:?}");
+                        }
+                        eprintln!("Unexpected error: {e:?}");
+                        abort = true;
+                        break;
+                    }
+                }
+
+                match reader.await_next_entry(None) {
+                    Err(e) => {
+                        if let Err(e2) = tx.blocking_send(Err(Status::cancelled("Unexpected error while waiting for journal entries"))) {
+                            eprintln!("Unexpected error while waiting for journal entries: {e:?}\nAlso an error occurred while cancelling stream: {e2:?}");
+                        }
+                        eprintln!("Unexpected error while waiting for journal entries: {e:?}");
+                        abort = true;
+                        break;
+                    },
+                    Ok(None) => {
+                        break;
+                        if let Err(e) = tx.blocking_send(Err(Status::cancelled("Unexpected end of journal"))) {
+                            eprintln!("Unexpected end of journal\nAlso, an error occurred while cancelling stream: {e:?}");
+                        }
+                        eprintln!("Unexpected end of journal");
+                        abort = true;
+                        break;
+                    },
+                    Ok(Some(record)) => {
+                        // if !filter_fields.iter().all(|(k, v)| {
+                        //     match record.get(k) {
+                        //         None => false,
+                        //         Some(val) => val == v,
+                        //     }
+                        // }) {
+                        //     continue;
+                        // }
+                        let record_map = record.into_iter().collect::<HashMap<String, String>>();
+                        let msg = record_map.get(KEY_MESSAGE).unwrap_or(&default_msg).clone();
+                        #[cfg(debug_assertions)]
+                        {
+                            println!("Sending journal msg: {msg}");
+                        }
+                        let log_msg = LogMessage {
+                            fields: record_map,
+                            message: msg,
+                            namespace: namespace.clone(),
+                        };
+                        if let Err(e) = tx.blocking_send(Ok(log_msg)) {
+                            eprintln!("Receiver has gone away, exiting ... ({e:?})");
+                            abort = true;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            if abort {
+                break;
+            }
+            if let Err(e) = reader.wait(None) {
+                eprintln!("Unexpected error while waiting for journal entries: {e:?}");
+            }
+        }
+    });
+
+    rx
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,176 +1,41 @@
 
 use std::collections::HashMap;
 use std::net::ToSocketAddrs;
-use std::thread;
-use tokio::sync::mpsc::Receiver;
-use tonic::{Request, Response, Status};
-//use errors::NcAtomicError;
-//use grpc_api::api::journal_log_stream_server::{JournalLogStream, JournalLogStreamServer};
-//use grpc_api::api::{LogFilter, LogMessage};
+use std::time::Duration;
 
-use tokio_stream::{wrappers::ReceiverStream, Stream, StreamExt};
+use tokio_stream::{Stream, StreamExt};
+use tonic::codegen::http::HeaderName;
 use tonic::transport::Server;
+use tower_http::cors::{AllowHeaders, AllowMethods, AllowOrigin, CorsLayer, ExposeHeaders};
+use grpc_journal::api::journal_log_stream_server::JournalLogStreamServer;
+use grpc_journal::JournalLogStreamService;
 
-pub mod api {
-    tonic::include_proto!("api");
-}
-use api::{LogMessage, LogFilter, journal_log_stream_server::JournalLogStreamServer, journal_log_stream_server::JournalLogStream};
-use grpc_journal::NcAtomicError;
-
-async fn get_log_stream(current_user: bool, system: bool, namespace: Option<String>, filter_fields: Option<HashMap<String, String>>) -> Receiver<Result<LogMessage, Status>> {
-    println!("getting log stream");
-
-    let (tx, rx) = tokio::sync::mpsc::channel(100);
-    let default_msg = "<no message>".to_string();
-
-    const KEY_UNIT: &str = "_SYSTEMD_UNIT";
-    const KEY_MESSAGE: &str = "MESSAGE";
-
-    thread::spawn(move || {
-        use systemd::journal::{self, JournalSeek};
-        let filter_fields = filter_fields.unwrap_or_default();
-        let mut opts = journal::OpenOptions::default()
-            .system(system)
-            .current_user(current_user)
-            .system(system).to_owned();
-        let reader_result = match &namespace {
-            None => opts
-                .all_namespaces(true)
-                .open(),
-            Some(ns) => opts.open_namespace(ns)
-        };
-        let mut reader = match reader_result {
-            Err(e) => {
-                eprintln!("Couldn't open journal: {e:?}");
-                return;
-            },
-            Ok(reader) => reader
-        };
-        for (k, v) in filter_fields.iter() {
-            if let Err(e) = reader.match_add(k, v.as_bytes()) {
-                eprintln!("Error adding filter: {e:?}");
-            }
-        }
-
-        reader.seek(JournalSeek::Tail).expect("Couldn't seek to end of journal");
-        if let Err(e) = reader.previous().map_err(|e| NcAtomicError::IOError(e.to_string())) {
-            eprintln!("{e:?}");
-            return;
-        };
-
-        let mut abort = false;
-        loop {
-            loop {
-                match reader.next() {
-                    Ok(0) => {
-                        // if let Err(e) = tx.blocking_send(Err(Status::cancelled("no more messages found"))) {
-                        //     eprintln!("An error occurred while cancelling stream: {e:?}");
-                        // }
-                        break;
-                    },
-                    Ok(_) => {},
-                    Err(e) => {
-                        if let Err(e2) = tx.blocking_send(Err(Status::cancelled("unexpected error"))) {
-                            eprintln!("Unexpected error: {e:?}\n Also, an error occurred while cancelling stream: {e2:?}");
-                        }
-                        eprintln!("Unexpected error: {e:?}");
-                        abort = true;
-                        break;
-                    }
-                }
-
-                match reader.await_next_entry(None) {
-                    Err(e) => {
-                        if let Err(e2) = tx.blocking_send(Err(Status::cancelled("Unexpected error while waiting for journal entries"))) {
-                            eprintln!("Unexpected error while waiting for journal entries: {e:?}\nAlso an error occurred while cancelling stream: {e2:?}");
-                        }
-                        eprintln!("Unexpected error while waiting for journal entries: {e:?}");
-                        abort = true;
-                        break;
-                    },
-                    Ok(None) => {
-                        break;
-                        if let Err(e) = tx.blocking_send(Err(Status::cancelled("Unexpected end of journal"))) {
-                            eprintln!("Unexpected end of journal\nAlso, an error occurred while cancelling stream: {e:?}");
-                        }
-                        eprintln!("Unexpected end of journal");
-                        abort = true;
-                        break;
-                    },
-                    Ok(Some(record)) => {
-                        // if !filter_fields.iter().all(|(k, v)| {
-                        //     match record.get(k) {
-                        //         None => false,
-                        //         Some(val) => val == v,
-                        //     }
-                        // }) {
-                        //     continue;
-                        // }
-                        let record_map = record.into_iter().collect::<HashMap<String, String>>();
-                        let msg = record_map.get(KEY_MESSAGE).unwrap_or(&default_msg).clone();
-                        #[cfg(debug_assertions)]
-                        {
-                            println!("Sending journal msg: {msg}");
-                        }
-                        let log_msg = LogMessage {
-                            fields: record_map,
-                            message: msg,
-                            namespace: namespace.clone(),
-                        };
-                        if let Err(e) = tx.blocking_send(Ok(log_msg)) {
-                            eprintln!("Receiver has gone away, exiting ... ({e:?})");
-                            abort = true;
-                            break;
-                        }
-                    }
-                }
-
-            }
-            if abort {
-                break;
-            }
-            if let Err(e) = reader.wait(None) {
-                eprintln!("Unexpected error while waiting for journal entries: {e:?}");
-            }
-        }
-    });
-
-    rx
-}
-
-#[derive(Debug, Default)]
-struct JournalLogStreamService {
-    user_logs: bool,
-    system_logs: bool
-}
-
-#[tonic::async_trait]
-impl JournalLogStream for JournalLogStreamService {
-    type TailStream = ReceiverStream<Result<LogMessage, Status>>;
-
-    async fn tail(&self, request: Request<LogFilter>) -> Result<Response<Self::TailStream>, Status> {
-        #[cfg(debug_assertions)]
-        {
-            println!("Received tail request: {:?}", request);
-        }
-
-        let filter = request.into_inner();
-
-        let rx = get_log_stream(self.user_logs, self.system_logs, filter.namespace, Some(filter.fields)).await;
-        Ok(Response::new(ReceiverStream::new(rx)))
-    }
-}
+const DEFAULT_MAX_AGE: Duration = Duration::from_secs(24 * 60 * 60);
+const DEFAULT_EXPOSED_HEADERS: [HeaderName; 3] = [
+    HeaderName::from_static("grpc-status"),
+    HeaderName::from_static("grpc-message"),
+    HeaderName::from_static("grpc-status-details-bin"),
+];
 
 async fn run_logstream_backend(user_logs: bool, system_logs: bool) -> Result<(), String> {
-    let service = JournalLogStreamService {user_logs, system_logs};
+    let service = JournalLogStreamService::new(user_logs, system_logs);
     Server::builder()
         .accept_http1(true)
-        .add_service(JournalLogStreamServer::new(service))
-        .serve("127.0.0.1:50051".to_socket_addrs().unwrap().next().unwrap())
+        .layer(CorsLayer::new()
+            // TODO: Enable CORS
+            .allow_origin(AllowOrigin::mirror_request())
+            .allow_headers(AllowHeaders::mirror_request())
+            .allow_methods(AllowMethods::mirror_request())
+            .allow_credentials(true)
+            .max_age(DEFAULT_MAX_AGE)
+            .expose_headers(ExposeHeaders::from(DEFAULT_EXPOSED_HEADERS)))
+        .add_service(tonic_web::enable(JournalLogStreamServer::new(service)))
+        .serve("0.0.0.0:50051".to_socket_addrs().unwrap().next().unwrap())
         .await
         .map_err(|e| e.to_string())?;
     Ok(())
 }
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -183,7 +48,9 @@ mod tests {
     use hyper_util::rt::TokioIo;
     use tonic::transport::{Endpoint, Uri};
     use tower::service_fn;
-    use super::api::journal_log_stream_client::JournalLogStreamClient;
+    use grpc_journal::get_log_stream;
+    use grpc_journal::api::journal_log_stream_client::JournalLogStreamClient;
+    use grpc_journal::api::LogFilter;
     use super::*;
 
     #[tokio::test]
@@ -204,10 +71,7 @@ mod tests {
     #[tokio::test]
     async fn test_consume_log_stream_grpc() {
         let (client, server) = tokio::io::duplex(1024);
-        let service = JournalLogStreamService {
-            user_logs: true,
-            system_logs: true
-        };
+        let service = JournalLogStreamService::new(true, true);
         tokio::spawn(async move {
             Server::builder()
                 .add_service(JournalLogStreamServer::new(service))
